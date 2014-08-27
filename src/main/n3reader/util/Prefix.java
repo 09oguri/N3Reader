3e524c0b0463c2f6222cfbd11ce5899c0a755068
09oguri <oguri.h.aa@m.titech.ac.jp>
2014-08-27 21:05:32 +0900

    refactoring Prefix class

--- src/main/n3reader/util/Prefix.java
+++ src/main/n3reader/util/Prefix.java
@@ -8,13 +8,18 @@ public class Prefix {
 
     public static String notOmit(String s, HashMap<String, String> uris) {
         String result = s;
-        int pos = result.indexOf(":");
 
-        if (pos > 0 && pos + 1 < result.length()) {
-            String prefix = result.substring(0, result.indexOf(":") + 1);
+        int pos = result.indexOf(":");
+        if (pos < 1) {
+            return result;
+        }
+
+        if (pos + 1 < result.length()) {
+            String prefix = result.substring(0, pos + 1);
+
             String uri = uris.get(prefix);
             if (uri != null) {
-                result = uri + result.substring(result.indexOf(":") + 1);
+                result = uri + result.substring(pos + 1);
             }
         }
 
--- test/test/n3reader/util/PrefixTest.java
+++ test/test/n3reader/util/PrefixTest.java
@@ -7,18 +7,24 @@ import java.util.HashMap;
 
 import main.n3reader.util.Prefix;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrefixTest {
+    private HashMap<String, String> uris;
+
+    @Before
+    public void setUp() {
+        this.uris = new HashMap<String, String>();
+        uris.put("dbpedia-ja:", "http://ja.dbpedia.org/resource/");
+    }
+
     @Test
     public void notOmitTest() {
         String expected = "http://ja.dbpedia.org/resource/ソメイヨシノ";
 
-        String encoded = "dbpedia-ja:ソメイヨシノ";
-        HashMap<String, String> uris = new HashMap<String, String>();
-        uris.put("dbpedia-ja:", "http://ja.dbpedia.org/resource/");
-
-        String actual = Prefix.notOmit(encoded, uris);
+        String omittedElm = "dbpedia-ja:ソメイヨシノ";
+        String actual = Prefix.notOmit(omittedElm, uris);
 
         assertThat(actual, is(expected));
     }
